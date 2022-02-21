e6fbb018e21906ff74f1487aaabcb78433550c88
jynnie <jynnie@mit.edu>
2022-02-20 23:31:53 -0800

    🍧 Fix point to juniper

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "juniper/style.css";
+import "juniper-ui/style.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/playground.tsx
+++ pages/playground.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Flex, Button } from "juniper";
+import { Box, Flex, Button } from "juniper-ui";
 
 export default function Playground() {
   return (
