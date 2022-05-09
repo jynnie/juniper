c868d71c690075954db2a1f77ec677ca9343cf52
jynnie <jynn.tang@gmail.com>
2022-05-08 23:11:10 -0700

    📙 Copy to clipboard documentation

---
+++ src/pages/helpers/copy-to-clipboard.tsx
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import {
+  Box,
+  Flex,
+  Text,
+  Heading,
+  ClickOutWrapper,
+  useClickOut,
+  CopyToClipboardWrapper,
+  Button,
+} from "components";
+import { sp } from "utils";
+
+import { Example } from "../../components/Example";
+import { useState } from "react";
+import { Clipboard } from "react-feather";
+
+export default function CopyToClipboardPage() {
+  return (
+    <>
+      <Head>
+        <title>Copy to Clipboard | 🌿🫐</title>
+      </Head>
+
+      <div>
+        <Heading h1>Copy to Clipboard</Heading>
+        <Heading h3 intent="secondary">
+          Save values to clipboard on click
+        </Heading>
+
+        <Flex col gap={sp("xxl")}>
+          <Example
+            description={
+              <>
+                Wrap elements in a{" "}
+                <Text code>{"<CopyToClipboardWrapper value={...}/>"}</Text> to
+                copy the value when clicking on the wrapped elements.
+              </>
+            }
+            code={`<CopyToClipboardWrapper value={"Hello"}>
+  <Button icon={<Clipboard />}>Copy Hello</Button>
+</CopyToClipboardWrapper>`}
+          >
+            <CopyToClipboardWrapper value={"Hello"}>
+              <Button icon={<Clipboard />}>Copy Hello</Button>
+            </CopyToClipboardWrapper>
+          </Example>
+        </Flex>
+      </div>
+    </>
+  );
+}
